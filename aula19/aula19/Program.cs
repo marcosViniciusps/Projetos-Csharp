623bd0d103743b521af4dd9d2ea87e054bd2ab28
Marcos Vinicius <74256777+marcosViniciusps@users.noreply.github.com>
2022-02-13 21:00:47 -0300

    Add files via upload

---
+++ aula19/aula19/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace aula19 {
+    class Program {
+        static void Main(string[] args) {
+
+            int a = 10;
+            int b = 2;
+            string c = "ABC";
+            int d = 0;
+            Console.WriteLine(a);
+
+            a *= a;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            Console.WriteLine(c);
+
+            c += "DEF";
+            Console.WriteLine(c);
+
+            d = a++;
+            Console.WriteLine(a);
+            Console.WriteLine(d);
+
+            d = ++a;
+            Console.WriteLine(a);
+            Console.WriteLine(d);
+
+
+
+        }
+    }
+}
---
+++ aula20/aula20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace aula20 {
+    class Program {
+        static void Main(string[] args) {
+
+            double a;
+            float b;
+            int c;
+            int d = 2;
+
+            a = 5.1;
+            b = (float)a;   // É necessário converter o valor para float
+            c = (int)a;   // É necessário converter o valor para int e mesmo assim ele sai truncado
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            double resultado = (double)c / d; // É necessário converter um dos valores para double para não perder informação
+            Console.WriteLine(resultado);
+        }
+    }
+}
