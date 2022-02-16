234c7e95099eebbb1b8535683b640c775dc013b4
Marcos Vinicius <74256777+marcosViniciusps@users.noreply.github.com>
2022-02-15 22:48:51 -0300

    Add files via upload

---
+++ cpt4_02/cpt4_02/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cpt4_02 {
+    class Funcionario {
+
+        public string nome;
+        public double salario;
+
+    }
+}
---
+++ cpt4_02/cpt4_02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace cpt4_02 {
+    class Program {
+        static void Main(string[] args) {
+
+            Funcionario f1, f2;
+            f1 = new Funcionario();
+            f2 = new Funcionario();
+
+            Console.WriteLine("Funcionário 1");
+            Console.Write("Digite o nome: ");
+            f1.nome = Console.ReadLine();
+            Console.Write("Digite o salário: ");
+            f1.salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Funcionário 2");
+            Console.Write("Digite o nome: ");
+            f2.nome = Console.ReadLine();
+            Console.Write("Digite o salário: ");
+            f2.salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Salário médio: {0}",(f1.salario+f2.salario)/2);
+
+        }
+    }
+}
